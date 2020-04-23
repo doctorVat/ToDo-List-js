f2c6aedd81f5407433b2806b2521d9664cc7d207
Greg <gggolub1@gmail.com>
2020-04-23 19:18:52 +0300

    1 commit

--- script.js
+++ script.js
@@ -2,13 +2,16 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 
 
 //event listeners
 todoButton.addEventListener('click', addTodo)
 
+todoList.addEventListener('click', deleteChecked)
 
+filterOption.addEventListener('click', filterTodo)
 
 //functions
 
@@ -37,8 +40,57 @@ function addTodo(event){
     todoList.appendChild(todoDiv);
 
     //clear todo-input value
-
     todoInput.value='';
 
 }
 
+function deleteChecked(e){
+    console.log(e.target);
+    const item = e.target;
+    //delete todo
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        
+        //animation
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function(){
+            todo.remove()
+        })
+    }
+
+    //checked mark
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+
+    }
+}
+
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case 'all':
+                todo.style.display = 'flex';
+                break;
+            case 'completed':
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+            case 'uncompleted':
+                if (!todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break; //не забываем break!!!!!!!!!!!!!
+        }
+    })
+}
+
+function saveLocalTodo(todo){
+    
+}
